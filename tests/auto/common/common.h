7cdae9ee7af0c31b52e91854a5b5452af94d3b0f
Andras Becsi <andras.becsi@digia.com>
2013-06-05 20:10:32 +0200

    Clean up leftover from old tests

--- tests/auto/common/common.h
+++ tests/auto/common/common.h
@@ -10,15 +10,9 @@ class EnginioObjectModel;
 
 namespace EnginioTests
 {
-
 const QString TESTAPP_ID(qgetenv("ENGINIO_BACKEND_ID"));
 const QString TESTAPP_SECRET(qgetenv("ENGINIO_BACKEND_SECRET"));
 const QString TESTAPP_URL(qgetenv("ENGINIO_API_URL"));
-
-const QString TEST_OBJECT_TYPE("objects.SelfLinkedObject");
-const QString CUSTOM_OBJECT_TYPE("objects.CustomObject");
-const int NETWORK_TIMEOUT = 5000;
-
 }
 
 #endif // ENGINIOTESTSCOMMON_H
