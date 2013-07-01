c6d431284bd2c38a835178e3b1d15c0bfc2a526b
Andras Becsi <andras.becsi@digia.com>
2013-07-01 19:44:35 +0200

    Fix the out-of-the-box experience for QtQuick examples on Windows
    On Windows the binaries are produced into the release/debug subfolder
    so we need to adjust the QML import path additionally.
    Also remove extra main.cpp and use the shared one with the todos example.

--- examples/quick/main.cpp
+++ examples/quick/main.cpp
@@ -11,6 +11,9 @@ int main(int argc, char* argv[])
 
     // This allows starting the example without previously defining QML2_IMPORT_PATH.
     QDir qmlImportDir(appPath);
+#if defined (Q_OS_WIN)
+    qmlImportDir.cd("..");
+#endif
     qmlImportDir.cd("../../../qml");
     view.engine()->addImportPath(qmlImportDir.canonicalPath());
     QObject::connect(view.engine(), SIGNAL(quit()), &app, SLOT(quit()));
