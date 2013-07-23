8bf6bd77fc5be6ea0f55805dbc81bd2ef7549987
Andras Becsi <andras.becsi@digia.com>
2013-07-23 19:16:53 +0200

    Do not leak EnginioReply in case of error.

--- src/enginio_client/enginiobackendconnection.cpp
+++ src/enginio_client/enginiobackendconnection.cpp
@@ -197,6 +197,12 @@ const QByteArray EnginioBackendConnection::generateBase64EncodedUniqueKey()
 
 void EnginioBackendConnection::onEnginioFinished(EnginioReply *reply)
 {
+    struct ReplyScope {
+        EnginioReply *_reply;
+        ReplyScope(EnginioReply *r) : _reply(r) { }
+        ~ReplyScope() { _reply->deleteLater(); }
+    } scope(reply);
+
     if (reply->isError()) {
         qDebug() << "\n\n### EnginioBackendConnection ERROR";
         qDebug() << reply->errorString();
@@ -216,8 +222,6 @@ void EnginioBackendConnection::onEnginioFinished(EnginioReply *reply)
 
     _socketUrl = QUrl(urlValue.toString());
     _tcpSocket->connectToHost(_socketUrl.host(), _socketUrl.port(8080));
-
-    reply->deleteLater();
 }
 
 void EnginioBackendConnection::protocolError(const char* message, WebSocketCloseStatus status)
